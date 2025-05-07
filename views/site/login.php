347db74b2c2fb49d4472ca730525cd67bc78a113
p2made <pedrofp@me.com>
2025-05-07 23:44:11 +1000

    Update login.php

--- views/site/login.php
+++ views/site/login.php
@@ -10,26 +10,48 @@ use yii\bootstrap5\Html;
 	<div class="row justify-content-center">
 		<div class="col-lg-5">
 			<div class="card shadow-lg border-0 rounded-lg mt-5">
-				<div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+				<div class="card-header">
+					<h3 class="text-center font-weight-light my-4">Login</h3>
+				</div>
 				<div class="card-body">
-					<form>
+					<?php $form = ActiveForm::begin([
+						'id'      => 'login-form',
+						'options' => ['class' => ''],
+					]); ?>
 						<div class="form-floating mb-3">
-							<input class="form-control" id="inputEmail" type="email" placeholder="name@example.com">
-							<label for="inputEmail">Email address</label>
+							<?= $form->field($model, 'username', [
+								'template' => "{input}\n{label}\n{error}",
+								'options'  => ['tag' => false],
+							])->textInput([
+								'class'       => 'form-control',
+								'id'          => 'inputEmail',
+								'type'        => 'email',
+								'placeholder' => 'name@example.com',
+								'autofocus'   => true,
+							])->label('Email address', ['for' => 'inputEmail']) ?>
 						</div>
 						<div class="form-floating mb-3">
-							<input class="form-control" id="inputPassword" type="password" placeholder="Password">
-							<label for="inputPassword">Password</label>
+							<?= $form->field($model, 'password', [
+								'template' => "{input}\n{label}\n{error}",
+								'options'  => ['tag' => false],
+							])->passwordInput([
+								'class'       => 'form-control',
+								'id'          => 'inputPassword',
+								'placeholder' => 'Password',
+							])->label('Password', ['for' => 'inputPassword']) ?>
 						</div>
 						<div class="form-check mb-3">
-							<input class="form-check-input" id="inputRememberPassword" type="checkbox" value="">
-							<label class="form-check-label" for="inputRememberPassword">Remember Password</label>
+							<?= Html::checkbox('remember', false, [
+								'class' => 'form-check-input',
+								'id'    => 'inputRememberPassword',
+							]) ?>
+							<?= Html::label('Remember Password', 'inputRememberPassword', ['class' => 'form-check-label']) ?>
 						</div>
 						<div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-							<a class="small" href="password.php">Forgot Password?</a>
-							<a class="btn btn-primary" href="index.php">Login</a>
+							<?= Html::a('Forgot Password?', ['site/password'], ['class' => 'small']) ?>
+							<?= Html::submitButton('Login', ['class' => 'btn btn-primary']) ?>
 						</div>
-					</form>
+					<?php ActiveForm::end(); ?>
 				</div>
 			</div>
 		</div>
